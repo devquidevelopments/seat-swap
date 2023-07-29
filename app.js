335f539bdf758851330979d97da3cf05e890ae04
Devqui <adil.mushtaq@devqui.dev>
2023-07-29 17:03:00 +0100

    Start google login

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var gauth = require('google-auth-library');
+var passport = require('passport');
+
+require('dotenv').config();
 
 // Modules
 var indexRouter = require('./routes/index');
@@ -26,12 +30,12 @@ app.use('/users', usersRouter);
 app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
