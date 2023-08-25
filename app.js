0b0c0a6fa47d22441b37d9fa1e17c71bb3b8b504
Devqui <adil.mushtaq@devqui.dev>
2023-08-25 14:12:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var Amadeus = require('amadeus');
 
+require('dotenv').config();
+var Amadeus = require('amadeus');
 var amadeus = new Amadeus({
   clientId: process.env.AMADEUS_KEY,
   clientSecret: process.env.AMADEUS_SECRET
